23f35b4f0d5266b31b82cdff8c22e838e843b293
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-06-20 11:28:40 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/org/junit/experimental/max/Categories.java
@@ -0,0 +1,69 @@
+package org.junit.experimental.max;
+
+public interface Categories {
+
+	interface CouldNotReadCoreException {
+
+	}
+
+	interface forFolder {
+
+	}
+
+	interface listener {
+
+	}
+
+	interface testComparator {
+
+	}
+
+	interface testStarted {
+
+	}
+
+	interface testFinished {
+
+	}
+
+	interface testFailure {
+
+	}
+
+	interface testRunFinished {
+
+	}
+
+	interface compare {
+
+	}
+
+	interface forFolder {
+
+	}
+
+	interface storedLocally {
+
+	}
+
+	interface run {
+
+	}
+
+	interface run {
+
+	}
+
+	interface run {
+
+	}
+
+	interface sortRequest {
+
+	}
+
+	interface sortedLeavesForTest {
+
+	}
+
+}
