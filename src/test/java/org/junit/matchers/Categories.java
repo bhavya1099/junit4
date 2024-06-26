66ac1d4ca4be5e13e75492751c47e2d28a9d7f02
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-06-26 12:37:25 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/org/junit/matchers/Categories.java
@@ -0,0 +1,45 @@
+package org.junit.matchers;
+
+public interface Categories {
+
+	interface hasItem {
+
+	}
+
+	interface hasItems {
+
+	}
+
+	interface everyItem {
+
+	}
+
+	interface containsString {
+
+	}
+
+	interface both {
+
+	}
+
+	interface either {
+
+	}
+
+	interface isThrowable {
+
+	}
+
+	interface isException {
+
+	}
+
+	interface tag2 {
+
+	}
+
+	interface tag3 {
+
+	}
+
+}
